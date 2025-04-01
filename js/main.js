a60745e7340823697671e48a64356d7aca0b376d
nxhuel <nahueltisera03@gmail.com>
2025-04-01 00:23:18 -0300

    ifts: avance frontend, arreglar links del header activados

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 // card links
 
-function changeImage(imageSrc) {
+function changeImage(imageSrc, cardTitle, cardText, linkHref) {
     document.getElementById("displayImage").src = imageSrc;
+    document.getElementById("cardTitle").textContent = cardTitle;
+    document.getElementById("cardText").textContent = cardText;
+    document.getElementById("cardLink").href = linkHref;
 }
